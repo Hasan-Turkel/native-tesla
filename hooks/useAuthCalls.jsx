1f5777b8b679c307eb4990b707d6f8469dfd3138
hasan <mmhotmail>
2024-03-21 17:28:06 +0300

    logout

--- hooks/useAuthCalls.jsx
+++ hooks/useAuthCalls.jsx
@@ -43,10 +43,10 @@ const useAuthCalls = (navigation) => {
       
     }
   };
-  const logout =  () => {
+  const logout = async() => {
 
     try {
-     
+      const { data } = await axiosWithToken.post(`/auth/logout`);
       setCurrentUser("")
       setCurrentId("")
     
